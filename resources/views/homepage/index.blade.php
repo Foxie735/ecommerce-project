6712279ef50bd0d9862a61657cc9cfa0005beb58
Foxie735 <muhammadaxel139@gmail.com>
2024-12-06 14:15:44 +0700

    New Commint

--- resources/views/homepage/index.blade.php
+++ resources/views/homepage/index.blade.php
@@ -6,9 +6,9 @@
         background-color: black;
     }
     .fixed-img {
-        height: 200px; /* Set a fixed height */
-        object-fit: cover; /* Ensure the image maintains its aspect ratio while covering the area */
-        width: 100%; /* Ensure the image takes the full width of the card */
+        height: 200px;
+        object-fit: cover; 
+        width: 100%;
     }
     .container {
         padding-left: 15px; /* Add padding to the left */
@@ -40,9 +40,9 @@
                 <h2 class="text-center">Product Category</h2>
                 <div class="category-divider"></div>
             </div>
-            <div class="row">
+            <div class="m-auto row">
                 {{-- First Category --}}
-                <div class="col-md-4">
+                <div class="col-4">
                     <div class="card mb-4 shadow-sm">
                         <a href="#">
                             <img src="{{ asset('assets/images/meals.jpg') }}" alt="Meals" class="card-img-top fixed-img">
@@ -55,7 +55,7 @@
                     </div>
                 </div>
                 {{-- Second Category --}}
-                <div class="col-md-4">
+                <div class="col-4">
                     <div class="card mb-4 shadow-sm">
                         <a href="#">
                             <img src="{{ asset('assets/images/drinks.jpg') }}" alt="Drinks" class="card-img-top fixed-img">
@@ -68,7 +68,7 @@
                     </div>
                 </div>
                 {{-- Third Category --}}
-                <div class="col-md-4">
+                <div class="col-4">
                     <div class="card mb-4 shadow-sm">
                         <a href="#">
                             <img src="{{ asset('assets/images/fruitsveges.jpg') }}" alt="Fruits & Vegges" class="card-img-top fixed-img">
@@ -181,5 +181,95 @@
         </div>
     </div>
     {{-- end promo product --}}
+    {{-- start new product --}}
+    <div class="container">
+        <div class="row mt-4">
+            <div class="col col-md-12 col-sm-12 mb-4">
+                <div class="category-divider"></div>
+                <h2 class="text-center">New Product</h2>
+                <div class="category-divider"></div>
+            </div>
+        {{-- first product --}}
+            <div class="col-md-4">
+                <div class="card mb-4 shadow-sm">
+                    <a href="#">
+                        <img src="{{ asset('assets/images/pizza1.jpg') }}" alt="" class="card-img-top fixed-img">
+                    </a>
+                    <div class="card-body">
+                        <a href="#" class="text-decoration-none">
+                            <p class="card-text">
+                                Pizza
+                            </p>
+                        </a>
+                        <div class="row mt-4">
+                            <div class="col">
+                                <button class="btn btn-light">
+                                    <i class="far fa-heart"></i>
+                                </button>
+                            </div>
+                            <div class="col-auto">
+                                <p>
+                                    Rp.100.000,00
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card mb-4 shadow-sm">
+                    <a href="#">
+                        <img src="{{ asset('assets/images/dumpling.jpg') }}" alt="" class="card-img-top fixed-img">
+                    </a>
+                    <div class="card-body">
+                        <a href="#" class="text-decoration-none">
+                            <p class="card-text">
+                                Dumpling
+                            </p>
+                        </a>
+                        <div class="row mt-4">
+                            <div class="col">
+                                <button class="btn btn-light">
+                                    <i class="far fa-heart"></i>
+                                </button>
+                            </div>
+                            <div class="col-auto">
+                                <p>
+                                    Rp.20.000,00
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card mb-4 shadow-sm">
+                    <a href="#">
+                        <img src="{{ asset('assets/images/drink6.jpg') }}" alt="" class="card-img-top fixed-img">
+                    </a>
+                    <div class="card-body">
+                        <a href="#" class="text-decoration-none">
+                            <p class="card-text">
+                                Tea
+                            </p>
+                        </a>
+                        <div class="row mt-4">
+                            <div class="col">
+                                <button class="btn btn-light">
+                                    <i class="far fa-heart"></i>
+                                </button>
+                            </div>
+                            <div class="col-auto">
+                                <p>
+                                    Rp.8.000,00
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    {{-- end new product --}}
     
 @endsection
