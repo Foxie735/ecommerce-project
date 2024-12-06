96a96bd2f312e115633425c307623e6f20c5da39
Foxie735 <muhammadaxel139@gmail.com>
2024-12-06 12:46:54 +0700

    New

--- resources/views/homepage/index.blade.php
+++ resources/views/homepage/index.blade.php
@@ -33,14 +33,13 @@
     {{-- end carousel --}}
 
     {{-- start product category --}}
-    <div class="row mt-4">
-        <div class="col col-md-12 col-sm-12 mb-4">
-            <div class="category-divider"></div>
-            <h2 class="text-center">Product Category</h2>
-            <div class="category-divider"></div>
-        </div>
-
-        <div class="container">
+    <div class="container">
+        <div class="row mt-4">
+            <div class="col col-md-12 col-sm-12 mb-4">
+                <div class="category-divider"></div>
+                <h2 class="text-center">Product Category</h2>
+                <div class="category-divider"></div>
+            </div>
             <div class="row">
                 {{-- First Category --}}
                 <div class="col-md-4">
@@ -87,13 +86,100 @@
     {{-- end product category --}}
 
     {{-- start promo product --}}
-    <div class="row mt-4">
-        <div class="col col-md-12 col-sm-12 mb-4">
-            <div class="category-divider"></div>
-            <h2 class="text-center">Promo</h2>
-            <div class="category-divider"></div>
+    <div class="container">
+        <div class="row mt-4">
+            <div class="col col-md-12 col-sm-12 mb-4">
+                <div class="category-divider"></div>
+                <h2 class="text-center">Promo</h2>
+                <div class="category-divider"></div>
+            </div>
+        {{-- first product --}}
+            <div class="col-md-4">
+                <div class="card mb-4 shadow-sm">
+                    <a href="#">
+                        <img src="{{ asset('assets/images/blueberry.jpg') }}" alt="" class="card-img-top fixed-img">
+                    </a>
+                    <div class="card-body">
+                        <a href="#" class="text-decoration-none">
+                            <p class="card-text">
+                                Blueberry
+                            </p>
+                        </a>
+                        <div class="row mt-4">
+                            <div class="col">
+                                <button class="btn btn-light">
+                                    <i class="far fa-heart"></i>
+                                </button>
+                            </div>
+                            <div class="col-auto">
+                                <p>
+                                    <del>Rp.15.000,00</del>
+                                    <br>
+                                    Rp.10.000,00
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card mb-4 shadow-sm">
+                    <a href="#">
+                        <img src="{{ asset('assets/images/chickensalad.jpg') }}" alt="" class="card-img-top fixed-img">
+                    </a>
+                    <div class="card-body">
+                        <a href="#" class="text-decoration-none">
+                            <p class="card-text">
+                                Chicken Salad
+                            </p>
+                        </a>
+                        <div class="row mt-4">
+                            <div class="col">
+                                <button class="btn btn-light">
+                                    <i class="far fa-heart"></i>
+                                </button>
+                            </div>
+                            <div class="col-auto">
+                                <p>
+                                    <del>Rp.30.000,00</del>
+                                    <br>
+                                    Rp.23.000,00
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card mb-4 shadow-sm">
+                    <a href="#">
+                        <img src="{{ asset('assets/images/drink5.jpg') }}" alt="" class="card-img-top fixed-img">
+                    </a>
+                    <div class="card-body">
+                        <a href="#" class="text-decoration-none">
+                            <p class="card-text">
+                                Juice
+                            </p>
+                        </a>
+                        <div class="row mt-4">
+                            <div class="col">
+                                <button class="btn btn-light">
+                                    <i class="far fa-heart"></i>
+                                </button>
+                            </div>
+                            <div class="col-auto">
+                                <p>
+                                    <del>Rp.12.000,00</del>
+                                    <br>
+                                    Rp.8.000,00
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-        {{  }}
     </div>
     {{-- end promo product --}}
+    
 @endsection
