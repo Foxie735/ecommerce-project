4218886a2d9e197a36c109738a17601aa8b8a323
ciitraa <ciitrardhanares@gmail.com>
2024-12-19 19:29:59 +0700

    19/12/24 (19.29)

--- resources/views/homepage/contact.blade.php
+++ resources/views/homepage/contact.blade.php
@@ -24,21 +24,35 @@
   <div class="container">
     <!-- Contact Information -->
     <hr>
-    <div class="text-center">
-      <h2 class="mb-4">Our Contact Information</h2>
-      <p><strong>Address:</strong> 123 E-Commerce Street, Online City, OC 45678</p>
-      <p><strong>Phone:</strong> +62 813-8172-1708</p>
-      <p><strong>Email:</strong> support@fuushop.com</p>
+    <div>
+      <h2 class=" text-center mb-4">Our Contact Information</h2>
+        <table >
+        <tr>
+          <th scope="row">Address</th>
+          <td>:</td>
+          <td></td>
+          <td>Jl. R.Mangun Muka Raya No.11, RT.11/RW.14, Rawamangun, Kec. Pulo Gadung, Kota Jakarta Timur, Daerah Khusus Ibukota Jakarta 13220</td>
+        </tr>
+        <tr>
+          <th scope="row">Phone</th>
+          <td>:</td>
+          <td></td>
+          <td>+62 813-8172-1708</td>
+        </tr>
+        <tr>
+          <th scope="row">Email</th>
+          <td>:</td>
+          <td></td>
+          <td>axelsavero@gmail.com</td>
+        </tr>
+      </table>
     </div>
 
     <!-- Map Section -->
     <hr>
     <div class="map">
       <h2 class="text-center mb-4">Find Us Here</h2>
-      <iframe 
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3162.920414939447!2d-122.08424968469491!3d37.42199977982544!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808fba4d1bcd62a7%3A0xa4e7f7e7f77de14c!2sGoogleplex!5e0!3m2!1sen!2sus!4v1633515267128!5m2!1sen!2sus" 
-        allowfullscreen="" loading="lazy">
-      </iframe>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.524925130941!2d106.87646897402476!3d-6.194253660682942!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f4ed14403213%3A0x2412a91a0f6a01c8!2sJakarta%20State%20University!5e0!3m2!1sen!2sid!4v1734610449093!5m2!1sen!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>
   </div>
 
