593cd25e57781feb36f6c7726a0b56fd2438c220
Foxie735 <muhammadaxel139@gmail.com>
2024-12-19 12:51:02 +0700

    19 December 2024 (12:50)

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -2,52 +2,40 @@
 @section('content')
 <div class="container-fluid">
     <div class="row">
-        <div class="col-6 col-lg-3">
+        <div class="col-8 col-lg-4">
             <div class="small-box bg-primary">
                 <div class="inner">
-                    <h3>150</h3>
-                    <p>New Order</p>
+                    <h3>{{ $itemorder }}</h3>
+                    <p>Order</p>
                 </div>
                 <div class="icon">
                     <i class="ion ion-bag"></i>
                 </div>
-                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                <a href="/admin/transaction" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
             </div>
         </div>
-        <div class="col-6 col-lg-3">
+        <div class="col-8 col-lg-4">
             <div class="small-box bg-info">
                 <div class="inner">
-                    <h3>150</h3>
+                    <h3>{{ $productCount }}</h3>
                     <p>Product</p>
                 </div>
                 <div class="icon">
                     <i class="ion ion-bag"></i>
                 </div>
-                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                <a href="/admin/product" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
             </div>
         </div>
-        <div class="col-6 col-lg-3">
+        <div class="col-8 col-lg-4">
             <div class="small-box bg-warning">
                 <div class="inner">
-                    <h3>150</h3>
+                    <h3>{{ $memberCount }}</h3>
                     <p>Member</p>
                 </div>
                 <div class="icon">
                     <i class="ion ion-bag"></i>
                 </div>
-                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-            </div>
-        </div>
-        <div class="col-6 col-lg-3">
-            <div class="small-box bg-success">
-                <div class="inner">
-                    <h3>150</h3>
-                    <p>Transaction</p>
-                </div>
-                <div class="icon">
-                    <i class="ion ion-bag"></i>
-                </div>
-                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                <a href="/admin/customer" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
             </div>
         </div>
     </div>
